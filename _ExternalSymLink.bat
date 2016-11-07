11c58f28e011e3100c57c0c4ac92e66c1443a938
Paul Demeulenaere <pauld@unity3d.com>
2016-11-07 17:13:03 +0100

    Minor : update bat tool for symlink (deprecated folder)

--- _ExternalSymLink.bat
+++ _ExternalSymLink.bat
@@ -1,5 +1,4 @@
 mklink /J "%cd%\Assets\UnityShaderEditor" "%cd%\External\MaterialGraphProject\Assets\UnityShaderEditor"
 mklink /J "%cd%\Assets\NewUI" "%cd%\External\MaterialGraphProject\Assets\NewUI"
 mklink /J "%cd%\Assets\GraphFramework" "%cd%\External\MaterialGraphProject\Assets\GraphFramework"
-mklink /J "%cd%\Assets\MaterialGraphInterface" "%cd%\External\MaterialGraphProject\Assets\MaterialGraphInterface"
 pause
