ee73508a1d596d8a92390cc24059800a7ab4a5f8
Andre McGrail <andrem@unity3d.com>
2018-08-15 14:55:38 +0200

    -Removed Light Table 'Shape' column as it's not needed in SRP dues to type having both 'Rectangle' and 'Disc' options -Added default handling of the shadow type as area lights couldn't have hard shadows

--- com.unity.render-pipelines.lightweight/LWRP/Editor/LightExplorer.cs
+++ com.unity.render-pipelines.lightweight/LWRP/Editor/LightExplorer.cs
@@ -63,25 +63,9 @@ namespace UnityEditor
 		{
 			return new[]
 			{
-				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Checkbox, Styles.On, "m_Enabled", 25), // 1: Enabled
-				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Name, Styles.Name, null, 200), // 0: Name
+				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Checkbox, Styles.On, "m_Enabled", 25), // 0: Enabled
+				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Name, Styles.Name, null, 200), // 1: Name
 				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Enum, Styles.Type, "m_Type", 120), // 2: Type
-				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Enum, Styles.Shape, "m_Type", 120, (r, prop, dep) =>
-				{
-					// This is only appliable to the Area lights that have a shape. For the other lights, nothing will be shown
-					if (Styles.LightShapeValues.Contains(prop.intValue))
-					{
-						EditorGUI.BeginProperty(r, GUIContent.none, prop);
-						EditorGUI.BeginChangeCheck();
-						int type = EditorGUI.IntPopup(r, prop.intValue, Styles.LightShapeTitles, Styles.LightShapeValues);
-
-						if (EditorGUI.EndChangeCheck())
-						{
-							prop.intValue = type;
-						}
-						EditorGUI.EndProperty();
-					}
-				}),      // 3: Shape
 				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Enum, Styles.Mode, "m_Lightmapping", 70, (r, prop, dep) =>
 				{
 					bool areaLight = dep.Length > 1 && (dep[0].enumValueIndex == (int)LightType.Area || dep[0].enumValueIndex == (int)LightType.Disc);
@@ -95,11 +79,31 @@ namespace UnityEditor
 							prop.intValue = newval;
 						}
 					}
-				}, null, null, new int[] { 2 }), // 4: Mode
-				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Color, Styles.Color, "m_Color", 70), // 5: Color
-				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Float, Styles.Intensity, "m_Intensity", 60), // 6: Intensity
-				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Float, Styles.IndirectMultiplier, "m_BounceIntensity", 110), // 7: Indirect Multiplier
-				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Enum, Styles.ShadowType, "m_Shadows.m_Type", 100), // 8: Shadow Type
+				}, null, null, new int[] { 2 }), // 3: Mode
+				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Color, Styles.Color, "m_Color", 70), // 4: Color
+				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Float, Styles.Intensity, "m_Intensity", 60), // 5: Intensity
+				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Float, Styles.IndirectMultiplier, "m_BounceIntensity", 110), // 6: Indirect Multiplier
+				new LightingExplorerTableColumn(LightingExplorerTableColumn.DataType.Enum, Styles.ShadowType, "m_Shadows.m_Type", 100, (r, prop, dep) =>
+				{
+					bool areaLight = dep.Length > 1 && (dep[0].enumValueIndex == (int)LightType.Rectangle || dep[0].enumValueIndex == (int)LightType.Disc);
+
+					if (areaLight)
+					{
+						EditorGUI.BeginProperty(r, GUIContent.none, prop);
+						EditorGUI.BeginChangeCheck();
+						bool shadows = EditorGUI.Toggle(r, prop.intValue != (int)LightShadows.None);
+
+						if (EditorGUI.EndChangeCheck())
+						{
+							prop.intValue = shadows ? (int)LightShadows.Soft : (int)LightShadows.None;
+						}
+						EditorGUI.EndProperty();
+					}
+					else
+					{
+						EditorGUI.PropertyField(r, prop, GUIContent.none);
+					}
+				}, null, null, new int[] { 2 }),     // 7: Shadow Type
 			};
 		}
 		
